2064457b84a02c17e33570f5fd34c08fc61264db
une10000 <alec1210@icloud.com>
2026-02-09 16:23:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def record_log(user, action):
 if st.session_state.user_auth is not None:
     with st.sidebar:
         # LOGO RCRP
-        st.image("https://media.discordapp.net/attachments/1441508709024006315/1467106550656270484/Capture_decran_2025-12-01_a_21.03.31.png", use_container_width=True)
+        st.image("https://media.discordapp.net/attachments/1441508709024006315/1467106550656270484/Capture_decran_2025-12-01_a_21.03.31.png?ex=698b0a73&is=6989b8f3&hm=76c2f537e9acfb1dd2c2dd1d930fa8e2cb88cce4e00e6109f20925d68d289d75&=&format=webp&quality=lossless&width=2732&height=1508", use_container_width=True)
         
         st.divider()
         # HEURE ET DATE A GAUCHE
