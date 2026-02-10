3bba50f2d358608c7c7162c344b0691398995200
une10000 <alec1210@icloud.com>
2026-02-10 15:57:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -319,7 +319,7 @@ if st.session_state.user_auth is None:
             st.rerun()
     with c2:
         st.markdown("### 👨‍🔧 AGENT RCT")
-        login_rct = st.text_input("Identifiant Agent", type="password", key="l_rct_ff")
+        login_rct = st.text_input("Identifiant Agent", placeholder= Code RCT", key="input_civil_align")type="password", key="l_rct_ff")
         if st.button("AUTHENTIFICATION RCT", key="b_rct_f", use_container_width=True):
             if login_rct == KEY_RCT:
                 st.session_state.user_auth = "RCT"
