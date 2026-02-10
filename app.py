06080824b76e344fe7a45da12c4acca76741a06c
une10000 <alec1210@icloud.com>
2026-02-10 13:58:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -744,7 +744,8 @@ with tabs[0]:
 
 with col_t:
     st.write("### 🖼️ APERÇU DU TITRE (LIVE)")
-    date_actuelle = datetime.now().strftime("%d/%m/%Y %H:%M")
+    # --- HEURE UTC+1 POUR L'AFFICHAGE ---
+    date_actuelle = (datetime.now() + timedelta(hours=1)).strftime("%d/%m/%Y %H:%M")
     nom_user = f_owner if f_owner != "---" else "---"
 # ... reste de tes variables
     marque_v = f_model if f_model else "---"
