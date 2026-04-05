09ef2da0e9bec31a5b97230e9f6a32f4f40b20c2
une10000 <alec1210@icloud.com>
2026-04-05 19:36:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ if st.session_state.user_auth is None:
             else: st.error("Clé invalide.")
 
     with c4:
-        st.markdown("### 🛡️ STAFF")
+        st.markdown("### 🛡️👮‍♂️ POLSTA")
         login_staff = st.text_input("Clé Maîtresse", placeholder="Code STAFF", type="password", key="l_st_ff")
         if st.button("ACCÈS ADMINISTRATEUR", key="b_st_f", use_container_width=True):
             if login_staff == KEY_STAFF:
